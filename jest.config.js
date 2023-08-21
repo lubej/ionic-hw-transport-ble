a0fc8352c299c6fd8fcaed7ee99d3a212a143bc9
lubej <9722540+lubej@users.noreply.github.com>
2023-08-21 19:02:09 +0200

    Add connect and disconnect test

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const config = {
+  rootDir: './',
+  modulePaths: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js'],
+  clearMocks: true,
+  resetMocks: true,
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': ['ts-jest'],
+  },
+  testPathIgnorePatterns: [
+    '/node_modules/',
+  ],
+  testTimeout: 60000,
+  target: 'es2020'
+}
+
+module.exports = config
